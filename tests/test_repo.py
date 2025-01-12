e62433deb3832fbacd6082924b23c0aba6d9ad6f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-12 21:12:39 +0400

    ci job

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,9 +1,18 @@
+import pytest
 from copier import run_copy
 
-def test_repo(tmp_path):
-    run_copy(".", tmp_path)
-    for file in tmp_path.iterdir():
-        for file in file.iterdir():
-            #if file.is_dir():
-            if file.is_file():
-                print(file)
+
+pytest.fixture()
+def repo(tmpdir):
+    run_copy(".", tmp_path, data={"project_name": "maffay"})
+    return tmpdir
+
+def test_repo(repo):
+    #run_copy(".", tmp_path, data={"project_name": "maffay"})
+
+    set([file for file in repo.rglob("*")])
+
+
+
+    #    for file in file.iterdir():
+    #        print(file)
