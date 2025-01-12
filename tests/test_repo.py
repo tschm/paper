d1d5c73938037323bbaadfa1eeeb38f02c11147b
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-12 21:15:39 +0400

    ci job

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -5,13 +5,12 @@ from copier import run_copy
 @pytest.fixture()
 def repo(tmp_path):
     run_copy(".", tmp_path, data={"project_name": "maffay"})
-    return tmp_path
+    return set([file for file in repo.rglob("*")])
 
 def test_repo(repo):
     #run_copy(".", tmp_path, data={"project_name": "maffay"})
 
-    s = set([file for file in repo.rglob("*")])
-	print(s)
+	print(repo)
 	assert False
 
 
