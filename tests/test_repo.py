9f636f6601808dfacc554005a203e24aaefa374f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-12 21:19:49 +0400

    ci job

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -4,8 +4,15 @@ from copier import run_copy
 
 @pytest.fixture()
 def repo(tmp_path):
+    # Assuming `run_copy` is copying a folder to `tmp_path`
     run_copy(".", tmp_path, data={"project_name": "maffay"})
-    return set([file for file in tmp_path.rglob("*")])
+
+    # Collect all files in the directory and return as a set of relative paths
+    # Use relative_to to get the file paths relative to `tmp_path`
+    return set(
+        [file.relative_to(tmp_path) for file in tmp_path.rglob("*") if file.is_file()]
+    )
+
 
 def test_repo(repo):
     #run_copy(".", tmp_path, data={"project_name": "maffay"})
