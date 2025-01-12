0b9f29117382ba205a9dff99e7e0ea8f748f86ac
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-12 21:15:04 +0400

    ci job

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -10,7 +10,9 @@ def repo(tmp_path):
 def test_repo(repo):
     #run_copy(".", tmp_path, data={"project_name": "maffay"})
 
-    set([file for file in repo.rglob("*")])
+    s = set([file for file in repo.rglob("*")])
+	print(s)
+	assert False
 
 
 
